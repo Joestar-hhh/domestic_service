4af7361f4646305ba7ebd88cb3ef2c26cb8c0dd8
joestar-hhh <1275058738@qq.com>
2020-06-18 01:46:37 +0800

    更新 后台部署

--- src/main/java/com/cykj/domestic/DomesticApplication.java
+++ src/main/java/com/cykj/domestic/DomesticApplication.java
@@ -3,12 +3,18 @@ package com.cykj.domestic;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DomesticApplication {
+public class DomesticApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DomesticApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DomesticApplication.class);
+    }
 }
