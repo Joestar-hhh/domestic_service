e2008e47d33a50c7f502b5a13dc702d8b174b45c
cjk992@qq.com <cjk5522196>
2020-06-22 15:00:41 +0800

    签到bug修复

--- src/main/java/com/cykj/domestic/mapper/UserClockMapper.java
+++ src/main/java/com/cykj/domestic/mapper/UserClockMapper.java
@@ -3,6 +3,7 @@ package com.cykj.domestic.mapper;
 
 import com.cykj.domestic.entity.UserClock;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Mapper
@@ -10,13 +11,13 @@ import org.springframework.stereotype.Repository;
 public interface UserClockMapper {
 
     // 查询用户积分总数
-    int queryIntegral(UserClock userClock);
+    int queryIntegral(int userId);
 
     // 查询连续签到天数
-    int queryContinuousSign(UserClock userClock);
+    int queryContinuousSign(int userId);
 
     // 新增签到数据
-    int insertSign(UserClock userClock);
+    int insertSign(@Param("e") UserClock userClock, @Param("userId") int userId);
 
     // 修改签到数据
     int updateSign(UserClock userClock);
--- src/main/java/com/cykj/domestic/service/UserClockService.java
+++ src/main/java/com/cykj/domestic/service/UserClockService.java
@@ -6,14 +6,17 @@ import com.cykj.domestic.util.ResultData;
 public interface UserClockService {
 
     // 查询用户积分总数
-    ResultData queryIntegral(UserClock userClock);
+    ResultData queryIntegral(int userId);
 
     //查询连续签到天数
-    ResultData queryContinuousSign(UserClock userClock);
+    ResultData queryContinuousSign(int userId);
 
     // 根据用户id查询签到数据
-    ResultData queryUser(UserClock userClock,int id);
+    ResultData queryUser(UserClock userClock,int userId);
 
     // 查询用户签到日期
     ResultData queryDate(int userId);
+
+    // 点击签到
+    ResultData updateSign(UserClock userClock);
 }
