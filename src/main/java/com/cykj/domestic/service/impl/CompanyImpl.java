dd1c9b8533ac61f766f50b09e3d96bb2b5608ec8
15160603831 <1571317440@qq.com>
2020-06-30 13:59:37 +0800

    更新服務器育嬰知識bug

--- src/main/java/com/cykj/domestic/service/impl/CompanyImpl.java
+++ src/main/java/com/cykj/domestic/service/impl/CompanyImpl.java
@@ -28,6 +28,7 @@ public class CompanyImpl implements CompanySrevice {
         Company company1 = companyMapper.companyLogin(company);
         if (company1 != null) {
             if (company1.getRoleId() != 3) {
+                // !3
                 if (!MD5Util.MakeMd5(company.getPwd()).equals(company1.getPwd())) {
                     resultData.setCode(1);
                     resultData.setMsg("密码错误");
